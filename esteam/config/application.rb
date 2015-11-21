41d5b9ecb63adf4bb2e285a0be294322f37d83b1
tarikm2 <tarikm2@uw.edu>
2015-11-21 09:13:38 +0000

    added Facebook.SDK.js, Heroku functionality

--- esteam/config/application.rb
+++ esteam/config/application.rb
@@ -2,6 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require 'rubygems'
+require 'bundler/setup'
+
+# require your gems as usual
+require 'nokogiri'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
